3cec968c405f8d792cb0f51d9a71614da236f8b2
thananjayan26 <jayanselva26@gmail.com>
2024-07-27 22:26:06 +0530

    camp, booking endpoints coded

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 // const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const campRoutes = require('./routes/campRoutes');
-// const traineeRoutes = require('./routes/traineeRoutes');
+const traineeRoutes = require('./routes/traineeRoutes');
 // const trainerRoutes = require('./routes/trainerRoutes');
 // const adminRoutes = require('./routes/adminRoutes');
 
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/camps', campRoutes);
-// app.use('/api/trainees', traineeRoutes);
+app.use('/api/trainees', traineeRoutes);
 // app.use('/api/trainers', trainerRoutes);
 
 module.exports = app;
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { protectRoute } = require('../middleware/protectRoute.js');
-const { createBooking, retrieveBooking } = require('../controllers/booking.controller.js');
+const { createBooking, retrieveBooking, retrieveAllBooking, cancelBooking, acceptBooking } = require('../controllers/booking.controller.js');
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createBooking);
-router.post("/retrieve", protectRoute, retrieveBooking);
+router.get("/retrieve", protectRoute, retrieveBooking);
+router.get("/retrieve-all", protectRoute, retrieveAllBooking);
+router.post("/cancel-booking", protectRoute, cancelBooking);
+router.post("/accept-booking", protectRoute, acceptBooking);
+
 module.exports = router;
